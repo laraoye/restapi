c2e7900b1e5c6ba3645ada06cd593f331c4d152f
laraoye <88383143+laraoye@users.noreply.github.com>
2023-03-01 12:26:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
   res.send([1,2,3])
 })
 
+app.get('/api/courses/:id', (req, res)  => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if(!course) return res.status(404).send('The course with the given ID was not found')
+  res.send(course);
+});
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
